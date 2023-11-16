f591a4e1c3adf8b79c2f28a4e0457c8cb66a1bba
Alexey Ivanov <>
2023-11-16 15:20:46 +0600

    added get_defillama_chain_tvl

---
+++ R/get_defillama_chain_tvl.R
@@ -0,0 +1,29 @@
+#' Chain TVL Data
+#'
+#' This function retrieves Total Value Locked (TVL) data for various chains
+#' from a specified API and formats it into a data.table for analysis.
+#'
+#' @return A data.table containing the TVL data with columns: name, symbol, tvl, gecko_id, cmcId, and chainId.
+#' The data is sorted by TVL in descending order.
+#' @export
+#' @examples
+#' chain_tvl = get_defillama_chain_tvl()
+#' head( chain_tvl )
+
+get_defillama_chain_tvl = function() {
+
+  # API request
+  request = paste0(  'https://api.llama.fi/chains' )
+  res = httr::GET( request )
+  content = httr::content( res, type = 'application/json' )
+
+  # Convert the list to a data.table,
+  tvl_chains  = rbindlist( content, fill = T )[ order( -tvl ) ]
+
+  # Reordering and renaming
+  setcolorder( tvl_chains, c( "name", "tokenSymbol", "tvl", "gecko_id", "cmcId", "chainId" ) )
+  setnames( tvl_chains, c( "tokenSymbol" ), c( "symbol" ) )
+
+  tvl_chains[]
+
+}
--- R/get_defillama_protocol_tvl.R
+++ R/get_defillama_protocol_tvl.R
@@ -1,19 +1,21 @@
-#' TVL Data from API
+#' Protocol TVL Data
 #'
 #' This function retrieves Total Value Locked (TVL) data for various protocols
 #' from a specified API and formats it into a data.table for analysis.
 #'
 #' @return A data.table containing TVL data with columns: symbol, tvl, slug, name, chain, and category.
+#' The data is sorted by TVL in descending order.
 #' @export
 #' @examples
-#' tvl = get_protocol_tvl()
+#' protocol_tvl = get_defillama_protocol_tvl()
+#' head( protocol_tvl )
 
-get_protocol_tvl = function() {
+get_defillama_protocol_tvl = function() {
 
   # API request
   request = 'https://api.llama.fi/protocols'
-  res = GET(request)
-  c = content(res, type = 'application/json')
+  res = httr::GET(request)
+  c = httr::content(res, type = 'application/json')
 
   # Using lapply to create a list of data.table objects
   tvl_protocols = lapply(c, function(x) {
